ad06c16b1ab1ab0b43108fc559b9796a1c9fed65
06000208 <06000208@users.noreply.github.com>
2021-01-23 08:52:58 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,26 +1,25 @@
-var vscode = require('vscode');
-var copy = require('copy-paste').copy;
+const vscode = require("vscode");
+const { copy } = require("copy-paste");
 
 function activate(context) {
-	var disposable = vscode.commands.registerCommand('copyRelativePath', function (uri) {
-		if (typeof uri === 'undefined') {
-			if (vscode.window.activeTextEditor) {
-				uri = vscode.window.activeTextEditor.document.uri;
-			}
-		}
-		if (!uri) {
-			vscode.window.showErrorMessage('Cannot copy relative path, as there appears no file is opened (or it is very large');
-			return;
-		}
-		var path = vscode.workspace.asRelativePath(uri);
-		path = path.replace(/\\/g, '/');
-		copy(path);
-	});
-	context.subscriptions.push(disposable);
+  const disposable = vscode.commands.registerCommand("copyEncodedWorkspacePath", function(uri) {
+    if (typeof uri === "undefined") {
+      if (vscode.window.activeTextEditor) {
+        uri = vscode.window.activeTextEditor.document.uri;
+      }
+    }
+    if (!uri) {
+      vscode.window.showErrorMessage("Cannot copy path, as there appears no file is opened (or it's very large");
+      return;
+    }
+    let path = vscode.workspace.asRelativePath(uri);
+    path = encodeURI("/" + path.replace(/\\/g, "/"));
+    copy(path);
+  });
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-
-function deactivate() {
-}
+// eslint-disable-next-line no-empty-function
+function deactivate() {}
 exports.deactivate = deactivate;
